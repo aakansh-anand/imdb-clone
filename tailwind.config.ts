83bd8ea80d4e392a56dea13e90e571dd6c5d9b7d
Aakansh Anand <aakanshanand15@gmail.com>
2024-12-31 15:06:58 +0530

    Initial Setup with Eslint, Prettier, File structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-code)"],
+      }
     },
   },
   plugins: [],
